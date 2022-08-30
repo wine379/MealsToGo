c6f120d65692223d86f18e2c15c201fa39a01977
wine379 <walusungu@gmail.com>
2022-08-30 14:51:26 +0200

    location fixes

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+ import { LocationContextProvider } from './src/services/location/location.context';
 const TAB_ICON = {
   Restaurants: "restaurant",
   Map: "map",
@@ -58,23 +59,25 @@ const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsContextProvider value={{ restaurants: [1, 2, 3] }}>
-          <NavigationContainer>
-            <Tab.Navigator
-              screenOptions={createScreenOptions}
-              tabBarOptions={{
-                activeTintColor: "tomato",
-                inactibeTintColor: "gray",
-              }}
-            >
-              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-              <Tab.Screen name="Map" component={MapScreen} />
-              <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </RestaurantsContextProvider>
+        <LocationContextProvider>
+          <RestaurantsContextProvider>
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={createScreenOptions}
+                tabBarOptions={{
+                  activeTintColor: 'tomato',
+                  inactiveTintColor: 'gray',
+                }}
+              >
+                <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
+                <Tab.Screen name='Map' component={Map} />
+                <Tab.Screen name='Settings' component={Settings} />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </RestaurantsContextProvider>
+        </LocationContextProvider>
       </ThemeProvider>
-      <ExpoStatusBar style="auto" />
+      <ExpoStatusBar style='auto' />
     </>
   );
 };
